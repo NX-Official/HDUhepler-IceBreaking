9f57efc162ddfe9ce1a416753c46009829130710
徐皓 <976180942@qq.com>
2021-10-07 19:31:04 +0800

    Update login.php

--- login.php
+++ login.php
@@ -6,23 +6,36 @@
 	<link rel="stylesheet" href="./mdui-v1.0.1/css/mdui.css"/>
 </head>
 
-<body>
+<body class="mdui-theme-primary-indigo mdui-theme-accent-pink">
 	<?php
+	function cleanInput($input)
+	{
+		$clean = strtolower($input);
+		$clean = preg_replace("/[^0-9]/", "", $clean);
+		$clean = substr($clean,0,12);
+		return $clean;
+	}
 	session_start();
 	if(!empty($_SESSION[login]))echo "<script>window.location.href='./index.php';</script>\n";
 	$MaxUid=(int)file_get_contents("./src/next_id.txt");
+	
+	$_GET[uid]=cleanInput($_GET[uid]);
+	$_GET[school_number]=cleanInput($_GET[school_number]);
+	
 	if(!empty($_GET[uid]))
 	{
 		if($_GET[uid]>=$MaxUid)
 		{
-			echo "<script>window.alert(\"User not found!\");</script>\n";
+			echo "<script>window.alert(\"不存在此用户!\");</script>\n";
 			echo "<script>window.location.href='./login.php';</script>\n";
+			die();
 		}
 		$_SESSION[uid]=$_GET[uid];
 		if($_GET[school_number]!=(int)file_get_contents("./src/$_SESSION[uid]/school_number.txt"))
 		{
-			echo "<script>window.alert(\"wrong school number!\");</script>\n";
+			echo "<script>window.alert(\"学号错误!\");</script>\n";
 			echo "<script>window.location.href='./login.php';</script>\n";
+			die();
 		}
 			
 		else 
@@ -33,16 +46,35 @@
 	}
 	else
 	{
+		//pattern="[0-9]+"
 		echo<<<'EOF'
+		
+		<div class="mdui-container doc-container">
+		
+		<h1 class="doc-title mdui-text-center">助手破冰认人脸系统</h1>
+		
 		<form name="input" action="./login.php" method="get">
-
-		UserID: <input type="text" name="uid">
-		School number: <input type="text" name="school_number">
-		<input type="submit" value="Submit">
-
-		</form>
+		<div class="mdui-textfield mdui-textfield-floating-label">
+		<label class="mdui-textfield-label">UserID</label>
+		<input class="mdui-textfield-input" type="text" name="uid"  required/>
+		<div class="mdui-textfield-error">编号只能包含数字</div>
+		<div class="mdui-textfield-helper">请输入您的用户编号</div>
+		  </div>
+		  <div class="mdui-textfield mdui-textfield-floating-label">
+		<label class="mdui-textfield-label">School number</label>
+		<input class="mdui-textfield-input" type="text" name="school_number" maxlength="8"  required/>
+		<div class="mdui-textfield-error">学号只能包含数字</div>
+		<div class="mdui-textfield-helper">请输入您的学号</div>
+		  </div>
+		  <br>
+		<input class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple"  type="submit" value="登录">
+		
+		
+		</div>
+			</form>
+			</div>
 EOF;
 	}
 	?>
-
+<script src="./mdui-v1.0.1/js/mdui.min.js"></script>
 </body>
